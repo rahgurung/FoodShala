162c4c71c10f2224903aa34c711c9042ec869983
Rahul Gurung <gurungrahul2@gmail.com>
2019-03-17 17:36:31 +0530

    added sql file

---
+++ foodshala.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 17, 2019 at 01:02 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.1.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodshala`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `people_id` int(11) NOT NULL,
+  `food_id` int(11) NOT NULL,
+  `restaurant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `people_id`, `food_id`, `restaurant_id`) VALUES
+(3, 1, 4, 2),
+(6, 1, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foods`
+--
+
+CREATE TABLE `foods` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `veg` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `foods`
+--
+
+INSERT INTO `foods` (`id`, `user_id`, `name`, `price`, `veg`) VALUES
+(1, 3, 'KFC Special Chicken', 500, 0),
+(2, 3, 'Chicken Legs', 300, 0),
+(3, 2, 'Cheese Pizza', 300, 1),
+(4, 2, 'Capsicum Pizza', 400, 1),
+(5, 3, 'KFC Special Burger', 150, 0),
+(6, 6, 'Fish Curry', 300, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `people_id` int(11) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `food_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `people_id`, `restaurant_id`, `food_id`) VALUES
+(1, 4, 3, 2),
+(2, 4, 3, 1),
+(3, 1, 2, 3),
+(4, 1, 2, 4),
+(5, 1, 3, 5),
+(6, 1, 3, 1),
+(7, 1, 3, 1),
+(8, 5, 3, 1),
+(9, 5, 3, 5),
+(10, 1, 6, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` int(1) NOT NULL,
+  `vegan` int(11) NOT NULL DEFAULT '2'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `type`, `vegan`) VALUES
+(1, 'rahul Gurung', 'rg@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1, 0),
+(2, 'Pizzahut', 'pizzahut@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 0, 2),
+(3, 'KFC', 'kfc@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 0, 2),
+(4, 'Riya Gurung', 'riya@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1, 0),
+(5, 'Kha jaunga', 'email@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1, 0),
+(6, 'Kha le', 'email2@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 0, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `foods`
+--
+ALTER TABLE `foods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `foods`
+--
+ALTER TABLE `foods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
