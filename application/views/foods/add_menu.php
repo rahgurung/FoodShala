9f3841040f6dcc9ca0f82cf7f86405ae8b4c121e
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-04-30 22:58:39 -0300

    Add a input file on form

--- application/views/foods/add_menu.php
+++ application/views/foods/add_menu.php
@@ -1,6 +1,6 @@
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('foods/add_menu'); ?>
+<?php echo form_open_multipart('foods/add_menu'); ?>
 <div class="row">
   <div class="col-md-4 col-md-offset-4">
     <h2 class="text-center"> <?= $title; ?> </h2>
@@ -14,11 +14,15 @@
     <option value="1">Veg</option>
     <option value="0">Non- Veg</option>
   </select>
-  </div>
+	</div>
   <div class="form-group">
     <label>Price</label>
     <input name="price" type="number" class="form-control" placeholder="Enter price.">
-  </div>
+	</div>
+	<div class="form-group">
+		<label>Image</label>
+		<input type="file" name="image" />
+	</div>
   <button type="submit" class="btn btn-primary btn-block">Add</button>
 </div>
 </div>
