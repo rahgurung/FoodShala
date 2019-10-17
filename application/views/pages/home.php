a5ac27571982e1d4330b4891cf0124c672b7cab2
Rahul Gurung <gurungrahul2@gmail.com>
2019-10-17 22:44:06 +0530

    design enhancement
    moved login and signup
    to center.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,9 +1,9 @@
 <div class="jumbotron">
-  <h2 class="display-4"> Welcome to <span style="font-family: 'Cookie', cursive; font-size:80px;">FoodShala</span> <br> Meals That Matter. ;)</h2>
+  <h2 class="display-4 text-center"> Welcome to <span style="font-family: 'Cookie', cursive; font-size:80px;">FoodShala</span> <br> Meals That Matter. ;)</h2>
   <?php if (!$this->session->userdata('logged_in')) : ?>
   <div class= "home_page">
-    <a class="btn btn-primary btn-block btn-lg" href="/users/register" role="button">Register</a>
-    <a class="btn btn-success btn-block btn-lg" href="/users/login" role="button">Login</a>
+    <button class="btn btn-primary home-button" href="/users/register" role="button">Register</button>
+    <button class="btn btn-success home-button" href="/users/login" role="button">Login</button>
   </div>
   <?php endif; ?>
 </div>
